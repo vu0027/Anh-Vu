96f2c1352b6ed4759559a532e028d421ed4513ab
Anh Vu <anhvu@Anhs-MacBook-Pro-2.local>
2023-08-20 18:28:19 -0500

    Fix path

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -41,7 +41,7 @@ const About: React.FC = () => {
                 <ProjectCard
                   title={project.title}
                   image={project.image}
-                  link={project.title === 'Which country is your ideal place to live?' ? undefined : project.link}
+                  link={project.link}
                 />
               )
             })};
